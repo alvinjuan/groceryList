20796b130ba32ed92e21f645e7e2d90582171493
Alvin J <alvinj@Alvins-MacBook-Pro.local>
2022-05-11 14:21:12 -0700

    init

---
+++ app.js
@@ -0,0 +1,17 @@
+
+// SELECTORS
+const btnList = document.querySelector(".btn-list");
+const inputList = document.querySelector(".input-list");
+
+// EVENT LISTENERS
+btnList.addEventListener("click", addToList);
+inputList.addEventListener("click", deleteList);
+
+// FUNCTIONS
+function addToList(){
+    
+}
+
+function deleteList(){
+
+}
